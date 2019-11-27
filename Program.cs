1a704f0360917d7b0cb19cb5e9c190b3af09f98b
Vignesh Hariharan Iyer <vigneshiyer666666@gmail.com>
2019-11-27 20:53:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.Write(i); //Prinitng 1
+            for(;i<5;i++)
+            {
+                Console.Write("\n"+i);
+                if(i>2)
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
