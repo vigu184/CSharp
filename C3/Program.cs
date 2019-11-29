8dec6898c096ef72880d2c549e42c8b797f780ab
Vignesh Hariharan Iyer <vigneshiyer666666@gmail.com>
2019-11-29 12:37:39 +0530

    Add files via upload

---
+++ C3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float d = 6/12 * 3; //Integer Ops
+            Console.WriteLine(d);
+        }
+    }
+}
